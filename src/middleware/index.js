f84bced8ec82695f04054cfe7ad9e80e7c60c619
cr0c0pk1 <alpics1337@gmail.com>
2021-10-22 15:48:30 +0300

    feat: add transaction middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,7 @@
 const networkMiddleware = require('./network');
 const blockMiddleware = require('./block');
 const addressMiddleware = require('./address');
+const transactionMiddleware = require('./transaction');
 
 module.exports = function (app) {
   app
@@ -8,5 +9,10 @@ module.exports = function (app) {
     .get('/blocks/hash/:hash', blockMiddleware.getBlockByHash)
     .get('/blocks/number/:blocknumber', blockMiddleware.getBlockByNumber)
     .get('/blocks/numbers/:blocknumber/:count', blockMiddleware.getXBlocksFromNthFromCChain)
-    .get('/address/hash/:hash', addressMiddleware.getAddressInfoByHash);
+    .get('/address/hash/:hash', addressMiddleware.getAddressInfoByHash)
+    .get('/transactions/hash/:hash', transactionMiddleware.getTransactionByHash)
+    .get('/transactions/:address/:n/:x', transactionMiddleware.getXTransactionsAfterNthFromAddress)
+    .get('/transactions/:n/:x', transactionMiddleware.getXPendingTransactionsAfterNth)
+    .get('/transactions/recentxchain', transactionMiddleware.getRecentTransactionsFromXChain)
+    .get('/transactions/recentpchain', transactionMiddleware.getRecentTransactionsFromPChain);
 };
