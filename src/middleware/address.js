3a90e52acfaf33957f93585879f78a7f23fb2dd5
cr0c0pk1 <alpics1337@gmail.com>
2021-10-22 15:40:23 +0300

    feat: add address middleware

---
+++ src/middleware/address.js
@@ -0,0 +1,46 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const cChainMethods = require('../modules/c-chain');
+const xChainMethods = require('../modules/x-chain');
+const pChainMethods = require('../modules/p-chain');
+
+const X_CHAIN = 'X';
+const P_CHAIN = 'P';
+const C_CHAIN = '0x';
+
+//GET address info by hash
+exports.getAddressInfoByHash = async (req, res, next) => {
+    let addressInfoFromXChain;
+    let addressInfoFromCChain;
+    let addressInfoFromPChain;
+
+    if ((req.params.hash).charAt(0) == X_CHAIN) {
+        addressInfoFromXChain = await xChainMethods.getAddressInfoByHashFromXChain(req.params.hash);
+
+        if (addressInfoFromXChain[0] == 1) {
+            res.send(addressInfoFromXChain[1]);
+        } else {
+            res.send(addressInfoFromXChain);
+        }
+    } else if ((req.params.hash).charAt(0) == P_CHAIN) {
+        addressInfoFromPChain = await pChainMethods.getAddressInfoFromPChain(req.params.hash);
+
+        if (addressInfoFromPChain[0] == 1) {
+            res.send(addressInfoFromPChain[1]);
+        } else {
+            res.send(addressInfoFromPChain[1]);
+        }
+    } else if ((req.params.hash).slice(0, 2) == C_CHAIN){
+        addressInfoFromCChain = await cChainMethods.getAddressInfoFromCChain(req.params.hash);
+
+        if (addressInfoFromCChain[0] == 1) {
+            res.send(addressInfoFromCChain[1])
+        } else {
+            res.send(addressInfoFromCChain);
+        }
+    } else {
+        res.send(JSON.parse('{"result":"wrong input"}'));
+    }
+};
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,10 +1,12 @@
 const networkMiddleware = require('./network');
 const blockMiddleware = require('./block');
+const addressMiddleware = require('./address');
 
 module.exports = function (app) {
   app
     .get('/network', networkMiddleware.getNetWorkActivity)
     .get('/blocks/hash/:hash', blockMiddleware.getBlockByHash)
     .get('/blocks/number/:blocknumber', blockMiddleware.getBlockByNumber)
-    .get('/blocks/numbers/:blocknumber/:count', blockMiddleware.getXBlocksFromNthFromCChain);
+    .get('/blocks/numbers/:blocknumber/:count', blockMiddleware.getXBlocksFromNthFromCChain)
+    .get('/address/hash/:hash', addressMiddleware.getAddressInfoByHash);
 };
